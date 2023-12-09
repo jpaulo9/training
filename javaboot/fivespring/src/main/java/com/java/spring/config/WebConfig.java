988a8451efab45b437f625689f6a320233fa18b4
João Paulo da Silva Araújo <joao.araujo@havan.com.br>
2023-12-09 14:59:56 -0300

    Provendo extensão XML e YAML, add serialization.

---
+++ javaboot/fivespring/src/main/java/com/java/spring/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.java.spring.config;
+
+import com.java.spring.serialization.converter.YamlJackson2HttpMessageConverter;
+import com.java.spring.util.MediaTypes;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+       converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        //QUERY PARAM
+//        configurer.favorParameter(true)
+//                .parameterName("MediaType").ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json",MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+        //HEADER PARAM
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json",MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML );
+
+
+    }
+}
---
+++ javaboot/fivespring/src/main/java/com/java/spring/util/MediaTypes.java
@@ -0,0 +1,8 @@
+package com.java.spring.util;
+
+public class MediaTypes {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String APPLICATION_YAML = "application/x-yaml";
+}
