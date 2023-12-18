ff15e6555b652cc6e3b4d1b558a9d2f8be0e6dce
João Paulo da Silva Araújo <joao.araujo@havan.com.br>
2023-12-18 07:41:55 -0300

    alterado porta dos testes

--- javaboot/fivespring/src/test/java/com/java/spring/config/TestConfigs.java
+++ javaboot/fivespring/src/test/java/com/java/spring/config/TestConfigs.java
@@ -2,7 +2,7 @@ package com.java.spring.config;
 
 public class TestConfigs {
 
-    public static final int SERVER_PORT = 8888;
+    public static final int SERVER_PORT = 80;
 
 
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
